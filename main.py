47cb0adde0afc044b3275ca626ba9f8bc0b13577
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-08 22:54:27 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# T-shirt problem:
+#
+# In order to make shirt A (sold for 80), a factory uses 1.5 meters of fabric,
+# 1.2 hours of cutting and 2 hours of sewing. And to make shirt B (sold for 120),
+# a factory spends 2 meters of fabric, 2 hours cutting and 3 hours sewing.
+# Taking into account that the factory has 27 meters of fabric, 24 hours of cut and
+# 45 hours of sewing per week, what is the ideal number of shirts A and B to be manufactured,
+# to make the most profit, assuming all of them will be sold?
+
+# Problema das camisetas:
+#
+# Para fazer a camisa A (vendida a 80 reais), uma fábrica gasta 1.5 metros de tecido,
+# 1.2 horsa de corte e 2horas de costura. Para fazer a camisa B (vendida a 120 reais),
+# uma fábrica gasta 2 metros de tecido, 2 horas de corte e 3 horas de costura.
+# Levando em conta que que a fábrica dispõe de 27 metros de tecido, 24 horas de corte
+# e 45 horas de costura por semana, qual o número ideal de camisas A e B a serem fabricadas,
+# para se obter o maior lucro, supondo que todas serão vendidas?
+
+fabric = 27
+cut = 24
+sewing = 45
+profit = 0
+list_a = []
+list_b = []
+shirt_a = 0
+shirt_b = 0
