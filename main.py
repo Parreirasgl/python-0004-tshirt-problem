c8670b46d8d895f290830be9fe219754ef57126a
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-08 23:09:06 -0300

    Random t-shirt and materials consumed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# I found a math problem and then thought it would be
+# possible to solve it through a computer simulation,
+# so i developed this simulation to solve it.
+# The problem is:
+
+# Encontrei um problema matemático e a seguir achei que seria
+# possível resolvê-lo através de uma simulação computacional,
+# de modo que desenvolvi essa simulação para resolvê-lo.
+# O problema é o seguinte:
+
 # T-shirt problem:
 #
 # In order to make shirt A (sold for 80), a factory uses 1.5 meters of fabric,
@@ -16,6 +26,8 @@
 # e 45 horas de costura por semana, qual o número ideal de camisas A e B a serem fabricadas,
 # para se obter o maior lucro, supondo que todas serão vendidas?
 
+from random import randint
+
 fabric = 27
 cut = 24
 sewing = 45
@@ -24,3 +36,30 @@ list_a = []
 list_b = []
 shirt_a = 0
 shirt_b = 0
+
+# Loop 100,000 simulations.
+# Fazer um loop com 100 mil simulações.
+for i in range(100000):
+
+# Keep a loop as long as there is material for a shirt (either A or B).
+# Manter um loop enquanto houver material para uma camisa (seja A ou B).
+    while (fabric >= 1.5 and cut >= 1.2 and sewing >= 2) \
+            or (fabric >= 2 and cut >= 2 and sewing >= 3):
+
+# Sort which type of shirt to manufacture A or B, discount the material,...
+# ...add the profit, and place the shirt in the appropriate shirt list.
+# Sortear qual tipo de camisa fabricar A ou B, descontar o material,...
+# ...adicionar o lucro, e colocar a camisa na lista de camisas apropriada.
+        r = randint(1, 2)
+        if r == 1:
+            fabric -= 1.5
+            cut -= 1.2
+            sewing -= 2
+            profit += 80
+            list_a.append("a")
+        else:
+            fabric -= 2
+            cut -= 2
+            sewing -= 3
+            profit += 120
+            list_a.append("b")
